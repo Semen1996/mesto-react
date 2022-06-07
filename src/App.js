2c58c6199847c8c909e0608e40fd36bbb61f15f2
Semen Tolstoguzov <semensemenovichsemenov2020@yandex.ru>
2022-06-07 18:12:00 +0300

    Первая сдача проектной работы №10

--- src/App.js
+++ src/App.js
@@ -36,14 +36,48 @@ function App() {
     setSelectedCard(card);
   }
 
+  const editPopupInputs = [
+    <>
+      <div className="popup__input">
+        <input id="name" className="popup__field popup__field_type_name" name="name" type="text" placeholder="Введите имя" minLength="2" maxLength="40" required />
+        <span className="popup__input-error name-error"></span>
+      </div>
+      <div className="popup__input">
+        <input id="about" class="popup__field popup__field_type_description" name="about" type="text" placeholder="Укажите кем работаете" minLength="2" maxLength="200" required />
+        <span className="popup__input-error about-error"></span>
+      </div>
+    </>
+  ];
+
+  const addPopupInputs = [
+    <>
+      <div className="popup__input">
+        <input id="name2" className="popup__field popup__field_type_title" name="name"  type="text" placeholder="Название" minLength="2" maxLength="30" required />
+        <span className="popup__input-error name2-error"></span>
+      </div>
+      <div className="popup__input">
+        <input id="link" className="popup__field popup__field_type_link" name="link" type="url" value="" placeholder="Ссылка на картинку" required />
+        <span className="popup__input-error link-error"></span>
+      </div>
+    </>
+  ];
+
+  const avatarPopupInputs = [
+      <div class="popup__input">
+        <input id="avatar" className="popup__field popup__field_type_avatar" name="avatar" type="url" placeholder="Ссылка на картинку" required />
+        <span className="popup__input-error avatar-error"></span>
+      </div>
+  ];
+
+
   return (
     <body className="root">
       <Header />
       <Main onEditProfile={handleEditProfileClick} onAddPlace={handleAddPlaceClick} onEditAvatar={handleEditAvatarClick} onCardClick={handleSelectedCard} />
       <Footer />
-      <PopupWithForm title="Редактировать профиль" name="edit" isOpen={isEditProfilePopupOpen} onClose={closeAllPopups}/>
-      <PopupWithForm title="Новое место" name="add" isOpen={isAddPlacePopupOpen} onClose={closeAllPopups}/>
-      <PopupWithForm title="Обновить аватар" name="updateAvatar" isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups}/>
+      <PopupWithForm title="Редактировать профиль" name="edit" isOpen={isEditProfilePopupOpen} onClose={closeAllPopups} children= {editPopupInputs} />
+      <PopupWithForm title="Новое место" name="add" isOpen={isAddPlacePopupOpen} onClose={closeAllPopups} children= {addPopupInputs} />
+      <PopupWithForm title="Обновить аватар" name="updateAvatar" isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups} children= {avatarPopupInputs} />
       <ImagePopup card={selectedCard} onClose={closeAllPopups}/>
     </body>
   );
@@ -51,3 +85,4 @@ function App() {
 
 
 export default App;
+
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -6,6 +6,7 @@ function PopupWithForm(props) {
 <div id="editButton" className={`popup popup_${props.name} ${props.isOpen && `popup_opened`}`} >
         <form className={`popup__container popup__container_${props.name}`} name="profile-form" novalidate>
           <h2 className="popup__title">{props.title}</h2>
+          {props.children}
           <button className="popup__submitButton" type="submit">Сохранить</button>
           <button className="popup__close" type="button" aria-label="Закрыть попап"  onClick={props.onClose}></button>
         </form>
